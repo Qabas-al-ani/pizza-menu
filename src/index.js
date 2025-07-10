d8000a2b3e20ac591a2239c2ae4f68f4c0bb4cdc
Qabas <qabasalani1@gmail.com>
2025-07-11 01:22:18 +0700

    added header, menu, and foooter component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,99 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
 function App() {
   return (
     <div>
-      <h1>Hello React!!</h1>
-      <Pizza />
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 }
 
+function Header() {
+  return <h1>React Pizza Co.</h1>;
+}
+function Menu() {
+  return (
+    <div>
+      <h1>Our Menu</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+function Footer() {
+  // return React.createElement("footer", null, "We're open");
+  // return <footer>{new Date().toLocaleTimeString()} We're open</footer>;
+
+  const hour = new Date().toLocaleTimeString();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  // if (hour >= openHour && hour <= closeHour) {
+  //   alert("we're open");
+  // } else {
+  //   alert("we're closed");
+  // }
+}
+
 function Pizza() {
-  return <h1>Pizza</h1>;
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="Delicious pepperoni pizza" />
+      <h1>Pizza Spinaci</h1>
+      <p>Tomato, mozarella, and pepperoni</p>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
